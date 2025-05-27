ea4cb89f70cbe431edc7e61b8fbb2624da937505
urewo396 <maks08@protonmail.com>
2025-05-27 18:17:03 +0200

    combine OCR and summarization into single /document-summary endpoint

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI, File, UploadFile
 import io
 from PIL import Image
 import pytesseract
+from transformers import pipeline
 
 app = FastAPI()
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-@app.post("/upload-image/")
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-async def upload_image(file: UploadFile = File(...)):
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
+@app.post("/document-summary/")
+async def image_summary(file: UploadFile = File(...)):
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes))
-
     imageContent = pytesseract.image_to_string(image)
-
-    return {"extracted_text": imageContent}
+    summary = summarizer(imageContent, max_length=150, min_length=40, do_sample=False)
+    summary = summary[0]['summary_text']
+    return{"summary": summary}

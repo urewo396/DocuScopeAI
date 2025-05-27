1b80646deb0eab800c7daa2fe404038bd8d8573c
urewo396 <maks08@protonmail.com>
2025-05-27 16:18:45 +0200

    Upload image function and converting image content into string

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import io
+from PIL import Image
+import pytesseract
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return("Hello world")
+@app.post("/upload-image/")
+
+async def upload_image(file: UploadFile = File(...)):
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes))
+
+    imageContent = pytesseract.image_to_string(image)
+
+    return {"extracted_text": imageContent}
